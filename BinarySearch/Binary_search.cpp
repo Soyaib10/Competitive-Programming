b7248ffcdbbc577a8bdc9ea5f92c467032b4334e
Soyaib Zihad <93576102+Soyaib10@users.noreply.github.com>
2023-04-15 22:56:07 +0600

    Update Binary_search.cpp

--- BinarySearch/Binary_search.cpp
+++ BinarySearch/Binary_search.cpp
@@ -12,7 +12,7 @@ int binarySearch(vector <int> &v, int x, int low, int high) {
     return -1;
 }
 
-int main(void) {
+int main() {
     vector <int> v = {1, 2, 3, 45};
     int x = 45, n = v.size(), result = binarySearch(v, x, 0, n - 1);
     if (result == -1) cout << -1;
