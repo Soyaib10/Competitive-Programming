84c722e5f15d320e98d5fc5aa332e4dff83debb3
Soyaib Zihad <93576102+Soyaib10@users.noreply.github.com>
2023-04-15 22:45:49 +0600

    Update nth_root.cpp

--- BinarySearch/nth_root.cpp
+++ BinarySearch/nth_root.cpp
@@ -1,44 +1,6 @@
-// Problem name:
-
 #include<bits/stdc++.h>
-
 using namespace std;
-
-#define fastIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
-#define MOD 1000000007
-#define MOD1 998244353
-#define INF 1e18
-#define nl "\n"
-#define PI 3.141592653589793238462
-
-typedef long long int ll;
-typedef unsigned long long ull;
-typedef long double lld;
-
-#ifndef ONLINE_JUDGE
-#define debug(x) cerr << #x <<": "; _print(x); cerr << endl;
-#else
-#define debug(x)
-#endif
-
-void _print(ll t) {cerr << t;}
-void _print(int t) {cerr << t;}
-void _print(string t) {cerr << t;}
-void _print(char t) {cerr << t;}
-void _print(lld t) {cerr << t;}
-void _print(double t) {cerr << t;}
-void _print(ull t) {cerr << t;}
-
-template <class T, class V> void _print(pair <T, V> p);
-template <class T> void _print(vector <T> v);
-template <class T> void _print(set <T> v);
-template <class T, class V> void _print(map <T, V> v);
-template <class T> void _print(multiset <T> v);
-template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
-template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+#define ll long long int
 
 ll n, m, k;
 double x, y;
@@ -46,9 +8,7 @@ vector <ll> v;
 
 bool ok(double mid, double y) {
     double val = 1;
-    for (int i = 0; i < y; i++) {
-        val *= mid;
-    }
+    for (int i = 0; i < y; i++) val *= mid;
     return val < x;
 }
 
@@ -62,13 +22,10 @@ void solve() {
     }
     cout << setprecision(10) << lo << nl;
 }
-
 int main() {
-#ifndef ONLINE_JUDGE
-    freopen("error.txt", "w", stderr);
-#endif
-    fastIO;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
 
     solve();
+    return 0;
 }
-
