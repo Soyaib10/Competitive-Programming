75a08535fdb9fe5bbaec2b139d2de753ec811ac1
Soyaib Zihad <93576102+Soyaib10@users.noreply.github.com>
2023-04-15 22:41:22 +0600

    Update binary_search_templete.cpp

--- BinarySearch/binary_search_templete.cpp
+++ BinarySearch/binary_search_templete.cpp
@@ -1,20 +1,16 @@
 // for real number
 for (int i = 0; i < 100; i++) {
-	double mid = (l + r) / 2;
+	double mid = (lo + hi) / 2;
 	if (ok(mid)) l = mid;
 	else r = mid;
 }
-cout << l << nl;
+cout << lo << "\n";
 
 ////// for general perpose////////
-
-
 // minimum value
-int lo = 0, hi = n, re = -1;
-
+ll lo = 0, hi = n, re = -1;
 while (lo <= hi) {
-	int mid = (lo + hi) / 2;
-
+	ll mid = (lo + hi) / 2;
 	if (ok(mid)) {
 		re = mid;
 		hi = mid - 1;
@@ -23,11 +19,9 @@ while (lo <= hi) {
 }
 
 // max value
-int lo = 0, hi = n, re = -1;
-
+ll lo = 0, hi = n, re = -1;
 while (lo <= hi) {
-	int mid = (lo + hi) / 2;
-
+	ll mid = (lo + hi) / 2;
 	if (ok(mid)) {
 		re = mid;
 		lo = mid + 1;
