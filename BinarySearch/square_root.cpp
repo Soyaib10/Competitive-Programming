931bb10e8312f41a967143dac7aa2ef3286ba764
Soyaib Zihad <93576102+Soyaib10@users.noreply.github.com>
2023-04-15 22:48:14 +0600

    Update square_root.cpp

--- BinarySearch/square_root.cpp
+++ BinarySearch/square_root.cpp
@@ -1,44 +1,6 @@
-// Problem name:
-
 #include<bits/stdc++.h>
-
+#define ll long long int
 using namespace std;
-
-#define fastIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
-#define MOD 1000000007
-#define MOD1 998244353
-#define INF 1e18
-#define nl "\n"
-#define PI 3.141592653589793238462
-
-typedef long long int ll;
-typedef unsigned long long ull;
-typedef long double lld;
-
-#ifndef ONLINE_JUDGE
-#define debug(x) cerr << #x <<": "; _print(x); cerr << endl;
-#else
-#define debug(x)
-#endif
-
-void _print(ll t) {cerr << t;}
-void _print(int t) {cerr << t;}
-void _print(string t) {cerr << t;}
-void _print(char t) {cerr << t;}
-void _print(lld t) {cerr << t;}
-void _print(double t) {cerr << t;}
-void _print(ull t) {cerr << t;}
-
-template <class T, class V> void _print(pair <T, V> p);
-template <class T> void _print(vector <T> v);
-template <class T> void _print(set <T> v);
-template <class T, class V> void _print(map <T, V> v);
-template <class T> void _print(multiset <T> v);
-template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
-template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
 
 ll n, m, k;
 double x;
@@ -56,15 +18,13 @@ void solve() {
         if(ok(mid)) lo = mid;
         else hi = mid;
     }
-    cout << setprecision(10) << lo << nl;
+    cout << setprecision(10) << lo << "\n";
 }
 
 int main() {
-#ifndef ONLINE_JUDGE
-    freopen("error.txt", "w", stderr);
-#endif
-    fastIO;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
 
     solve();
+    return 0;
 }
-
