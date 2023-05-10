80f8aca05b105aad6dff66bf2110286ea45191f7
Soyaib Zihad <93576102+Soyaib10@users.noreply.github.com>
2023-05-10 13:59:05 +0600

    Update Counting_occurrences_using _prefix_sum_2D.cpp

--- Prefix-sum and Difference-array/Counting_occurrences_using _prefix_sum_2D.cpp
+++ Prefix-sum and Difference-array/Counting_occurrences_using _prefix_sum_2D.cpp
@@ -1,3 +1,4 @@
+// https://codeforces.com/contest/1363/problem/B
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long int
@@ -25,10 +26,44 @@ void solve() {
 }
 
 void oslve() {
+
     for (int i = 1; i <= n; i++) {
         for (int j = 0; j < 2; j++) prefix[i][j] = prefix[i - 1][j];
         prefix[i][ch[i - 1] - '0']++;
     }
+}
+
+ll prefix[2000][2];
+
+void solve() {
+    string s;
+    cin >> s;
+
+    int n = s.size();
+
+    vector <int> zero, one;
+    int count0 = 0, count1 = 0;
+    for (int i = 0; i < n; i++) {
+        for(int j = 0; j < 2; j++) prefix[i + 1][j] = prefix[i][j];
+
+        prefix[i + 1][s[i] - '0']++;
+    }
+
+    int ans = min(prefix[n][0], prefix[n][1]);
+
+    for(int i = 1; i <= n; i++){
+        /// for leftzero with index i
+        int x = prefix[i][1];
+        int y = prefix[n][0] - prefix[i][0];
+
+        ans = min(ans, x + y);
+
+        x = prefix[i][0];
+        y = prefix[n][1] - prefix[i][1];
+
+        ans = min(ans, x + y);
+    }
+    cout << ans << endl;
 }
 
 int main() {
