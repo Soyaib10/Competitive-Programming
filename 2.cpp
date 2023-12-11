9de2585e7d68b76f80872a3e0d1c1d3b65a632aa
Soyaib10 <soyaibzihad10@gmail.com>
2023-12-12 02:32:32 +0600

    v-1, introduction to DP

--- 2.cpp
+++ 2.cpp
@@ -5,8 +5,13 @@ using namespace std;
 void solve() {
     int n;
     cin >> n;
-    vector<int> v(n);
-    for (int i = 0; i < n; i++) cin >> v[i];
+    int last2 = 0, last = 1;
+    for (int i = 2; i <= n; i++) {
+        int cur = last2 + last;
+        last2 = last;
+        last = cur;
+    }
+    cout << last << "\n";
 }
 
 int main() {
--- Dynamic Programming/1.2-Fibonacci-Tebular.cpp
+++ Dynamic Programming/1.2-Fibonacci-Tebular.cpp
@@ -2,17 +2,15 @@
 #define ll long long int
 using namespace std;
 
-int f(int n, vector<int>& dp) {
-    if (n <= 1) return n;
-    if (dp[n] == -1) dp[n] = f(n - 1, dp) + f(n - 2, dp);
-    return dp[n];
-}
-
 void solve() {
     int n;
     cin >> n;
     vector<int> dp(n + 1, -1);
-    cout << f(n, dp);
+    
+    dp[0] = 0;
+    dp[1] = 1;
+    for (int i = 2; i <= n; i++) dp[i] = dp[i - 1] + dp[i - 2];
+    cout << dp[n] << "\n";
 }
 
 int main() {
